dff33fc1ac97a98120f0e10b6122b9b1a60282a7
神崎航 <kanzwataru@gmail.com>
2019-01-05 17:33:53 -0500

    * added firing

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	public GameObject projectilePrefab;
+	public float force = 300f;
+	public float upForceMult = 1.5f;
+	public float cooldownTime = 3f;
+
+	float cooldownTimer = 0f;
+	bool firing = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update() {
+		cooldownTimer = Mathf.Clamp(cooldownTimer + Time.deltaTime, 0f, cooldownTime);
+
+		if(firing) {
+			if(cooldownTimer >= cooldownTime) {
+				cooldownTimer = 0.0f;
+				FireProjectile();
+			}
+		}
+
+		/* DEBUG */
+		firing = Input.GetKey(KeyCode.Space);	
+	}
+
+	void FireProjectile() {
+
+		var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0,0,0)).GetComponent<Rigidbody>();
+		projectile.AddForce(force, force * upForceMult, 0);
+	}
+}
---
+++ Assets/Scripts/ZKill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZKill : MonoBehaviour {
+	public float height = -30f;
+	Transform xform;
+
+	// Use this for initialization
+	void Start () {
+		xform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(xform.position.y < height) {
+			Destroy(gameObject);
+		}
+	}
+}
