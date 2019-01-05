d639ec5296a0f8828efdcf0a9ce17e1045ef0072
Neighto <nathanmladd@gmail.com>
2019-01-05 16:30:21 -0500

    change

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    private float horizMove;
+    public float shipSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizMove = Input.GetAxis("Horizontal");
+
+
+	}
+}
