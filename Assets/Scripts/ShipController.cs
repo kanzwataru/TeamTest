96f971569efdfa1f51199d0697f1536a5b22bdef
Neighto <nathanmladd@gmail.com>
2019-01-05 17:30:34 -0500

    Ship moving

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
 
-    private float horizMove;
-    public float shipSpeed = 3f;
+    public bool playerTwo;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float horizMove;
+    private bool flipShip = false;
+    public float shipSpeed = 3f;
+    private Transform sprite;
+
+    private void Awake()
+    {
+        sprite = this.transform.GetChild(0);
+    }
+
+    private void FlipShip()
+    {
+        flipShip = !flipShip;
+        sprite.Rotate(new Vector3(0,0,180));
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        horizMove = Input.GetAxis("Horizontal");
+        //Player One
+        if (playerTwo == false)
+        {
+            horizMove = Input.GetAxis("Horizontal") * shipSpeed * Time.deltaTime;
+        }
+        else //Player Two
+        {
+            horizMove = Input.GetAxis("Horizontal2") * shipSpeed * Time.deltaTime;
+        }
 
+        if (horizMove > 0 && flipShip == false)
+        {
+            FlipShip();
+        }
+        else if (horizMove < 0 && flipShip == true)
+        {
+            FlipShip();
+        }
+        transform.Translate(horizMove, 0, 0);
 
 	}
 }
