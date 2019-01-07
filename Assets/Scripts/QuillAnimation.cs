a489ad9de72b92b1838c10fca5c69bf7634b61cb
神崎航 <kanzwataru@gmail.com>
2019-01-06 23:19:20 -0500

    Improved QuillAnimation performance * Moved frame counters out into a QuillAnimSystem singleton class.
      It doesn't really make sense to have every single animation count the frame times
      so instead they just ask if it's ready to switch frames for their specific FPS.
    
    * Use a fixed-length array for the layer frames instead of List<T>
      (probably doesn't make a huge difference
       but we don't ever change the number of frames per layer so why not)

--- Assets/Scripts/QuillAnimation.cs
+++ Assets/Scripts/QuillAnimation.cs
@@ -3,38 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuillAnimation : MonoBehaviour {
-    public float frameRate = 12;
+    public int frameRate = 12;
 
-    private List<GameObject> layers = new List<GameObject>();
-    private float frameTimeCounter = 0.0f;
+    private GameObject[] layers;
     private int frame = 0;
 
 	// Use this for initialization
 	void Start () {
+        QuillAnimSystem.instance.RequestFPSCounter(frameRate);
+
+        layers = new GameObject[transform.childCount];
         for (int i = 0; i < transform.childCount; i++)
         {
-            layers.Add(transform.GetChild(i).gameObject);
+            layers[i] = transform.GetChild(i).gameObject;
             layers[i].SetActive(false);
         }
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Benchmarker.instance.BenchCall("QuillAnimation Update", () => {
-            frameTimeCounter += (Time.deltaTime * 1000);
-            if(frameTimeCounter >= 1000 / frameRate) {
-                layers[frame].SetActive(false);
+        if(QuillAnimSystem.instance.ShouldUpdate(frameRate)) {
+            layers[frame].SetActive(false);
 
-                if (++frame == layers.Count) //up frames
-                {
-                    frame = 0;
-                }
-
-                layers[frame].SetActive(true);
-
-                frameTimeCounter = 0.0f;
+            if (++frame == layers.Length) {
+                frame = 0;
             }
-        });
+
+            layers[frame].SetActive(true);
+        }
 	}
 }
